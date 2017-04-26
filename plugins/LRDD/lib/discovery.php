ea6d8b8bdeae63181efcbf7f4fd17c1450acdbd1
Mikael Nordfeldth <mmn@hethane.se>
2017-04-26 23:21:13 +0200

    LRDD blacklisted URL test

--- plugins/LRDD/lib/discovery.php
+++ plugins/LRDD/lib/discovery.php
@@ -140,6 +140,11 @@ class Discovery
                 $xrd->loadString($response->getBody(), $type);
                 return $xrd;
 
+            } catch (ClientException $e) {
+                if ($e->getCode() === 403) {
+                    common_log(LOG_INFO, sprintf('%s: Aborting discovery on URL %s: %s', _ve($class), _ve($uri), _ve($e->getMessage())));
+                    break;
+                }
             } catch (Exception $e) {
                 common_log(LOG_INFO, sprintf('%s: Failed for %s: %s', _ve($class), _ve($uri), _ve($e->getMessage())));
                 continue;
