1049080df53fd36168650bdd626784972df30227
Diogo Cordeiro <diogo@fc.up.pt>
2018-07-20 23:00:18 -0600

    [CORE] Move public resources to a /public directory Advantages: * Increases security by preventing direct access to file/ * We are careful and have a defined('GNUSOCIAL') || die() to prevent   direct access to GS files, but we may miss one or a vendor/extlib may   not be as careful * Improves directory structure - It's more natural to physically   separate what is public from what are GNU social resources

--- public/index.php
+++ public/index.php
@@ -1,40 +1,50 @@
 <?php
+// This file is part of GNU social - https://www.gnu.org/software/social
+//
+// GNU social is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GNU social is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with GNU social.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * StatusNet - the distributed open-source microblogging tool
- * Copyright (C) 2008, 2009, StatusNet, Inc.
+ * Main GNU social entry point
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @category StatusNet
- * @package  StatusNet
- * @author   Brenda Wallace <shiny@cpan.org>
- * @author   Brion Vibber <brion@pobox.com>
- * @author   Christopher Vollick <psycotica0@gmail.com>
- * @author   CiaranG <ciaran@ciarang.com>
- * @author   Craig Andrews <candrews@integralblue.com>
- * @author   Evan Prodromou <evan@controlezvous.ca>
- * @author   Gina Haeussge <osd@foosel.net>
- * @author   James Walker <walkah@walkah.net>
- * @author   Jeffery To <jeffery.to@gmail.com>
- * @author   Mike Cochrane <mikec@mikenz.geek.nz>
- * @author   Robin Millette <millette@controlyourself.ca>
- * @author   Sarven Capadisli <csarven@controlyourself.ca>
- * @author   Tom Adams <tom@holizz.com>
- * @author   Zach Copley <zach@status.net>
- * @copyright 2009 Free Software Foundation, Inc http://www.fsf.org
- *
- * @license  GNU Affero General Public License http://www.gnu.org/licenses/
+ * @package   GNUsocial
+ * @author    Brenda Wallace <shiny@cpan.org>
+ * @author    Brion Vibber <brion@pobox.com>
+ * @author    Brion Vibber <brion@status.net>
+ * @author    Christopher Vollick <candrews@integralblue.com>
+ * @author    CiaranG <ciaran@ciarang.com>
+ * @author    Craig Andrews <candrews@integralblue.com>
+ * @author    Craig Andrews <evan@status.net>
+ * @author    Evan Prodromou <evan@controlezvous.ca>
+ * @author    Evan Prodromou <evan@controlyourself.ca>
+ * @author    Evan Prodromou <evan@prodromou.name>
+ * @author    Evan Prodromou <evan@status.net>
+ * @author    Gina Haeussge <osd@foosel.net>
+ * @author    James Walker <walkah@walkah.net>
+ * @author    Jeffery To <candrews@integralblue.com>
+ * @author    Jeffery To <jeffery.to@gmail.com>
+ * @author    Mikael Nordfeldth <mmn@hethane.se>
+ * @author    Mike Cochrane <mikec@mikenz.geek.nz>
+ * @author    Robin Millette <millette@controlyourself.ca>
+ * @author    Sarven Capadisli <csarven@controlyourself.ca>
+ * @author    Sarven Capadisli <csarven@status.net>
+ * @author    Siebrand Mazeland <s.mazeland@xs4all.nl>
+ * @author    Tom Adams <candrews@integralblue.com>
+ * @author    Tom Adams <tom@holizz.com>
+ * @author    Zach Copley <zach@status.net>
+ * @author    Diogo Cordeiro <diogo@fc.up.pt>
+ * @copyright 2019 Free Software Foundation, Inc http://www.fsf.org
+ * @license   https://www.gnu.org/licenses/agpl.html GNU AGPL v3 or later
  */
 
 $_startTime = microtime(true);
