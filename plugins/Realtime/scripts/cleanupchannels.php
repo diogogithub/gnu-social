c4f962a7d096d60a5cfd1872d695b0f0bb76347c
Diogo Cordeiro <diogo@fc.up.pt>
2019-04-25 22:32:30 +0100

    Fix wrong path on Realtime cleanupchannels script by XRevan86

--- plugins/Realtime/scripts/cleanupchannels.php
+++ plugins/Realtime/scripts/cleanupchannels.php
@@ -20,7 +20,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-define('INSTALLDIR', realpath(dirname(__FILE__) . '/../..'));
+define('INSTALLDIR', realpath(__DIR__ . '/../../..'));
 
 $shortoptions = 'u';
 $longoptions = array('universe');
