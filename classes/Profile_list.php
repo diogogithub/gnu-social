diogogithub/gnu-social

commit 9a39ebe66f11a5a0c8158d83bc37dec207b9cb8c

Database fields with timestamp type now allow CURRENT_TIMESTAMP to be set as default value Add default values to created and modified columns of Profile_list table to fix issue #338

classes/Profile_list.php
@@ -48,8 +48,8 @@ class Profile_list extends Managed_DataObject
                 'description' => array('type' => 'text', 'description' => 'description of the people tag'),
                 'private' => array('type' => 'int', 'size' => 'tiny', 'default' => 0, 'description' => 'is this tag private'),
 
-                'created' => array('type' => 'timestamp', 'not null' => true, 'description' => 'date the tag was added'),
-                'modified' => array('type' => 'timestamp', 'not null' => true, 'description' => 'date the tag was modified'),
+                'created' => array('type' => 'timestamp', 'not null' => true, 'default' => 'CURRENT_TIMESTAMP', 'description' => 'date the tag was added'),
+                'modified' => array('type' => 'timestamp', 'not null' => true, 'default' => 'CURRENT_TIMESTAMP', 'description' => 'date the tag was modified'),
 
                 'uri' => array('type' => 'varchar', 'length' => 191, 'description' => 'universal identifier'),
                 'mainpage' => array('type' => 'varchar', 'length' => 191, 'description' => 'page to link to'),

lib/schema.php
@@ -790,7 +790,7 @@ class Schema
 
     function quoteDefaultValue($cd)
     {
-        if ($cd['type'] == 'datetime' && $cd['default'] == 'CURRENT_TIMESTAMP') {
+        if (($cd['type'] == 'datetime' || $cd['type'] == 'timestamp') && $cd['default'] == 'CURRENT_TIMESTAMP') {
             return $cd['default'];
         } else {
             return $this->quoteValue($cd['default']);

commit 306d80de9496a0287cb84a398488bd7e106cd516

[DATABASE] Revert accidental regression introduced with 9a39ebe66f

classes/Profile_list.php
@@ -48,7 +48,7 @@ class Profile_list extends Managed_DataObject
                 'description' => array('type' => 'text', 'description' => 'description of the people tag'),
                 'private' => array('type' => 'int', 'size' => 'tiny', 'default' => 0, 'description' => 'is this tag private'),
 
-                'created' => array('type' => 'timestamp', 'not null' => true, 'default' => 'CURRENT_TIMESTAMP', 'description' => 'date the tag was added'),
+                'created' => array('type' => 'timestamp', 'not null' => true, 'description' => 'date the tag was added'),
                 'modified' => array('type' => 'timestamp', 'not null' => true, 'default' => 'CURRENT_TIMESTAMP', 'description' => 'date the tag was modified'),
 
                 'uri' => array('type' => 'varchar', 'length' => 191, 'description' => 'universal identifier'),

lib/framework.php
@@ -32,7 +32,7 @@ defined('GNUSOCIAL') || die();
 define('GNUSOCIAL_ENGINE', 'GNU social');
 define('GNUSOCIAL_ENGINE_URL', 'https://www.gnu.org/software/social/');
 
-define('GNUSOCIAL_BASE_VERSION', '1.20.1');
+define('GNUSOCIAL_BASE_VERSION', '1.20.2');
 define('GNUSOCIAL_LIFECYCLE', 'release'); // 'dev', 'alpha[0-9]+', 'beta[0-9]+', 'rc[0-9]+', 'release'
 
 define('GNUSOCIAL_VERSION', GNUSOCIAL_BASE_VERSION . '-' . GNUSOCIAL_LIFECYCLE);