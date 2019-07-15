36dd245ee373178fbec9fb017ebc72c212eb896b
Diogo Cordeiro <diogo@fc.up.pt>
2019-07-15 01:29:39 +0100

    [DOCUMENTATION] New socialfy solution

--- DOCUMENTATION/SYSTEM_ADMINISTRATORS/socialfy-another-domain/dot-well-known/webfinger/index.php
+++ DOCUMENTATION/SYSTEM_ADMINISTRATORS/socialfy-another-domain/dot-well-known/webfinger/index.php
@@ -1,43 +1,56 @@
 <?php
 
-/*
- * GNU social
- * Copyright (C) 2010, Free Software Foundation, Inc
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// FIXME: REPLACE          \/ here
+define('MY_GNUSOCIAL', 'https://www.example.org/gnusocial/index.php');
+
+/**
+ * This is a general solution for when you can't have your GNU social instance in the domain root and for when you want to
+ * socialfy from another domain.
  */
 
-
-// basename should make sure we can't escape this directory
-$u = basename($_GET['resource']);
-
-if (!strpos($u, '@')) {
-    throw new Exception('Bad resource');
-    exit(1);
+// From https://www.php.net/manual/en/function.getallheaders.php#84262 (joyview at gmail dot com)
+if (!function_exists('getallheaders')) {
+    function getallheaders()
+    {
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
 }
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_HTTPHEADER, getallheaders());
+curl_setopt($ch, CURLOPT_URL, MY_GNUSOCIAL . str_replace('webfinger/', 'webfinger', $_SERVER['REQUEST_URI']));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, true);
+$response = curl_exec($ch);
+$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+$body = substr($response, $header_size);
+// From https://stackoverflow.com/a/10590242 (c.hill)
+function get_headers_from_curl_response($response)
+{
+    $headers = array();
 
-if (mb_strpos($u, 'acct:')===0) {
-    $u = substr($u, 5);
+    $header_text = substr($response, 0, strpos($response, "\r\n\r\n"));
+
+    foreach (explode("\r\n", $header_text) as $i => $line) {
+        if ($i === 0) {
+            $headers['http_code'] = $line;
+        } else {
+            list($key, $value) = explode(': ', $line);
+
+            $headers[$key] = $value;
+        }
+    }
+
+    return $headers;
 }
-
-// Just to be a little bit safer, you know, with all the unicode stuff going on
-$u = filter_var($u, FILTER_SANITIZE_EMAIL);
-
-$f = $u . ".xml";
-
-if (file_exists($f)) {
-  header('Content-Disposition: attachment; filename="'.urlencode($f).'"');
-  header('Content-type: application/xrd+xml');
-  echo file_get_contents($f);
+$headers = get_headers_from_curl_response($response);
+foreach ($headers as $name => $value) {
+    header("{$name}: $value");
 }
+echo $body;
+curl_close($ch);
