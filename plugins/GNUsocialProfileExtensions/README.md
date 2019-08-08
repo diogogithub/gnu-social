90bd9088bbcb261f1803a27f650ba7533cc05ec1
Diogo Cordeiro <diogo@fc.up.pt>
2019-08-08 15:08:23 +0100

    [GNUsocialProfileExtensions] Fixed plugin

--- plugins/GNUsocialProfileExtensions/README.md
+++ plugins/GNUsocialProfileExtensions/README.md
@@ -1,15 +1,14 @@
-GNU Social Profile Extensions
+GNU social Profile Extensions
 =============================
 
 Allows administrators to define additional profile fields for the
-users of a GNU Social installation.
+users of a GNU social installation.
 
 
 Installation
 ------------
 
-To install, copy this directory into your plugins directory and add
-the following lines to your config.php file:
+To enable, add the following lines to your config.php file:
 
 addPlugin('GNUsocialProfileExtensions');
 $config['admin']['panels'][] = 'profilefields';
--- plugins/GNUsocialProfileExtensions/lib/profiletools.php
+++ plugins/GNUsocialProfileExtensions/lib/profiletools.php
@@ -1,47 +1,52 @@
 <?php
+// This file is part of GNU social - https://www.gnu.org/software/social
+//
+// GNU social is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GNU social is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with GNU social.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * GNU Social
- * Copyright (C) 2010, Free Software Foundation, Inc.
- *
- * PHP version 5
- *
- * LICENCE:
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Allows administrators to define additional profile fields for the users of a GNU social installation.
  *
  * @category  Widget
- * @package   GNU Social
+ * @package   GNU social
  * @author    Max Shinn <trombonechamp@gmail.com>
- * @copyright 2010 Free Software Foundation, Inc.
- * @license   http://www.fsf.org/licensing/licenses/agpl-3.0.html AGPL 3.0
+ * @author    Diogo Cordeiro <diogo@fc.up.pt>
+ * @copyright 2011-2019 Free Software Foundation, Inc http://www.fsf.org
+ * @license   https://www.gnu.org/licenses/agpl.html GNU AGPL v3 or later
  */
+
+defined('GNUSOCIAL') || die();
 
 function gnusocial_profile_merge(&$profile)
 {
     $responses = GNUsocialProfileExtensionResponse::findResponsesByProfile($profile->id);
-    $profile->customfields = array();
+    $profile->customfields = [];
     foreach ($responses as $response) {
         $title = $response->systemname;
         $profile->$title = $response->value;
         $profile->customfields[] = $title;
     }
 }
-    
+
 function gnusocial_field_systemname_validate($systemname)
 {
+    // Ensure it doesn't exist already
     $fields = GNUsocialProfileExtensionField::allFields();
-    foreach ($fields as $field)
-        if ($field->systemname == $systemname)
+    foreach ($fields as $field) {
+        if ($field->systemname == $systemname) {
             return false;
-    return ctype_alphanum($systemname);
+        }
+    }
+    // Ensure that it consist of only alphanumeric characters
+    return ctype_alnum($systemname);
 }
