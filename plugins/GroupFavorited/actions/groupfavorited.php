2d7ca37036dcc5e5c24bfc90b7c2d993f8f2392b
Diogo Cordeiro <diogo@fc.up.pt>
2019-08-10 20:21:09 +0100

    [GroupFavorited] Fix plugin

--- plugins/GroupFavorited/actions/groupfavorited.php
+++ plugins/GroupFavorited/actions/groupfavorited.php
@@ -1,36 +1,29 @@
 <?php
+// This file is part of GNU social - https://www.gnu.org/software/social
+//
+// GNU social is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GNU social is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with GNU social.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * StatusNet, the distributed open-source microblogging tool
+ * The GroupFavorited plugin adds a menu item for popular notices in groups.
  *
- * List of popular notices
- *
- * PHP version 5
- *
- * LICENCE: This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @category  Public
- * @package   StatusNet
- * @author    Zach Copley <zach@status.net>
- * @author    Evan Prodromou <evan@status.net>
- * @copyright 2008-2009 StatusNet, Inc.
- * @license   http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License version 3.0
- * @link      http://status.net/
+ * @package   GroupFavoritedPlugin
+ * @author    Brion Vibber <brion@status.net>
+ * @copyright 2010-2019 Free Software Foundation, Inc http://www.fsf.org
+ * @license   https://www.gnu.org/licenses/agpl.html GNU AGPL v3 or later
  */
 
-if (!defined('STATUSNET') && !defined('LACONICA')) {
-    exit(1);
-}
+defined('GNUSOCIAL') || die();
 
 class GroupFavoritedAction extends ShowgroupAction
 {
@@ -38,8 +31,9 @@ class GroupFavoritedAction extends ShowgroupAction
      * Title of the page
      *
      * @return string page title, with page number
+     * @throws Exception
      */
-    function title()
+    public function title()
     {
         $base = $this->group->getFancyName();
 
@@ -48,9 +42,11 @@ class GroupFavoritedAction extends ShowgroupAction
             return sprintf(_m('Popular posts in %s group'), $base);
         } else {
             // TRANS: %1$s is a group name, %2$s is a group number.
-            return sprintf(_m('Popular posts in %1$s group, page %2$d'),
-                           $base,
-                           $this->page);
+            return sprintf(
+                _m('Popular posts in %1$s group, page %2$d'),
+                $base,
+                $this->page
+            );
         }
     }
 
@@ -61,24 +57,26 @@ class GroupFavoritedAction extends ShowgroupAction
      *
      * @return void
      */
-    function showContent()
+    public function showContent()
     {
-        $groupId = intval($this->group->id);
+        $groupId = (int)$this->group->id;
         $weightexpr = common_sql_weight('fave.modified', common_config('popular', 'dropoff'));
-        $cutoff = sprintf("fave.modified > '%s'",
-                          common_sql_date(time() - common_config('popular', 'cutoff')));
+        $cutoff = sprintf(
+            "fave.modified > '%s'",
+            common_sql_date(time() - common_config('popular', 'cutoff'))
+        );
 
-        $qry = 'SELECT notice.*, '.
-          $weightexpr . ' as weight ' .
-          'FROM notice ' .
-          "JOIN group_inbox ON notice.id = group_inbox.notice_id " .
-          'JOIN fave ON notice.id = fave.notice_id ' .
-          "WHERE $cutoff AND group_id = $groupId " .
-          'GROUP BY id,profile_id,uri,content,rendered,url,created,notice.modified,reply_to,is_local,source,notice.conversation ' .
-          'ORDER BY weight DESC';
+        $qry = 'SELECT notice.*, ' .
+            $weightexpr . ' as weight ' .
+            'FROM notice ' .
+            "JOIN group_inbox ON notice.id = group_inbox.notice_id " .
+            'JOIN fave ON notice.id = fave.notice_id ' .
+            "WHERE $cutoff AND group_id = $groupId " .
+            'GROUP BY id,profile_id,uri,content,rendered,url,created,notice.modified,reply_to,is_local,source,notice.conversation ' .
+            'ORDER BY weight DESC';
 
         $offset = ($this->page - 1) * NOTICES_PER_PAGE;
-        $limit  = NOTICES_PER_PAGE + 1;
+        $limit = NOTICES_PER_PAGE + 1;
 
         if (common_config('db', 'type') == 'pgsql') {
             $qry .= ' LIMIT ' . $limit . ' OFFSET ' . $offset;
@@ -86,20 +84,26 @@ class GroupFavoritedAction extends ShowgroupAction
             $qry .= ' LIMIT ' . $offset . ', ' . $limit;
         }
 
-        $notice = Memcached_DataObject::cachedQuery('Notice',
-                                                    $qry,
-                                                    600);
+        $notice = Memcached_DataObject::cachedQuery(
+            'Notice',
+            $qry,
+            600
+        );
 
         $nl = new NoticeList($notice, $this);
 
         $cnt = $nl->show();
 
-        if ($cnt == 0) {
-            //$this->showEmptyList();
-        }
+        /*if ($cnt == 0) {
+            $this->showEmptyList();
+        }*/
 
-        $this->pagination($this->page > 1, $cnt > NOTICES_PER_PAGE,
-                          $this->page, 'groupfavorited',
-                          array('nickname' => $this->group->nickname));
+        $this->pagination(
+            $this->page > 1,
+            $cnt > NOTICES_PER_PAGE,
+            $this->page,
+            'groupfavorited',
+            array('nickname' => $this->group->nickname)
+        );
     }
 }
