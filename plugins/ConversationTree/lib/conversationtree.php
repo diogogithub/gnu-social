68db757269a5ab9e5a9fba7596ef7319b6c2c90d
Diogo Cordeiro <diogo@fc.up.pt>
2019-08-11 02:18:50 +0100

    [ConversationTree] Format the plugin, add strict typing and fix docblocks

--- plugins/ConversationTree/lib/conversationtree.php
+++ plugins/ConversationTree/lib/conversationtree.php
@@ -1,64 +1,50 @@
 <?php
-/**
- * StatusNet - the distributed open-source microblogging tool
- * Copyright (C) 2011, StatusNet, Inc.
- *
- * Conversation tree widget for oooooold school playas
- * 
- * PHP version 5
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @category  Widget
- * @package   StatusNet
- * @author    Evan Prodromou <evan@status.net>
- * @copyright 2011 StatusNet, Inc.
- * @license   http://www.fsf.org/licensing/licenses/agpl-3.0.html AGPL 3.0
- * @link      http://status.net/
- */
+// This file is part of GNU social - https://www.gnu.org/software/social
+//
+// GNU social is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GNU social is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with GNU social.  If not, see <http://www.gnu.org/licenses/>.
 
-if (!defined('GNUSOCIAL')) { exit(1); }
+defined('GNUSOCIAL') || die();
 
 /**
  * Conversation tree
  *
  * The widget class for displaying a hierarchical list of notices.
  *
- * @category Widget
- * @package  StatusNet
- * @author   Evan Prodromou <evan@status.net>
- * @license  http://www.fsf.org/licensing/licenses/agpl.html AGPLv3
- * @link     http://status.net/
+ * @category  Widget
+ * @package   ConversationTreePlugin
+ * @author    Evan Prodromou <evan@status.net>
+ * @copyright 2019 Free Software Foundation, Inc http://www.fsf.org
+ * @license   https://www.gnu.org/licenses/agpl.html GNU AGPL v3 or later
  */
 class ConversationTree extends NoticeList
 {
-    var $tree  = null;
-    var $table = null;
+    public $tree = null;
+    public $table = null;
 
     /**
      * Show the tree of notices
      *
-     * @return void
+     * @return int
      */
-    function show()
+    public function show(): int
     {
         $cnt = $this->_buildTree();
 
-        $this->out->elementStart('div', array('id' =>'notices_primary'));
+        $this->out->elementStart('div', ['id' => 'notices_primary']);
         // TRANS: Header on conversation page. Hidden by default (h2).
         $this->out->element('h2', null, _('Notices'));
-        $this->out->elementStart('ol', array('class' => 'notices xoxo old-school'));
+        $this->out->elementStart('ol', ['class' => 'notices xoxo old-school']);
 
         if (array_key_exists('root', $this->tree)) {
             $rootid = $this->tree['root'][0];
@@ -71,28 +57,27 @@ class ConversationTree extends NoticeList
         return $cnt;
     }
 
-    function _buildTree()
+    public function _buildTree(): int
     {
         $cnt = 0;
 
-        $this->tree  = array();
-        $this->table = array();
+        $this->tree = [];
+        $this->table = [];
 
         while ($this->notice->fetch()) {
-
             $cnt++;
 
-            $id     = $this->notice->id;
+            $id = $this->notice->id;
             $notice = clone($this->notice);
 
             $this->table[$id] = $notice;
 
             if (is_null($notice->reply_to)) {
-                $this->tree['root'] = array($notice->id);
-            } else if (array_key_exists($notice->reply_to, $this->tree)) {
+                $this->tree['root'] = [$notice->id];
+            } elseif (array_key_exists($notice->reply_to, $this->tree)) {
                 $this->tree[$notice->reply_to][] = $notice->id;
             } else {
-                $this->tree[$notice->reply_to] = array($notice->id);
+                $this->tree[$notice->reply_to] = [$notice->id];
             }
         }
 
@@ -106,12 +91,12 @@ class ConversationTree extends NoticeList
      *
      * @return void
      */
-    function showNoticePlus($id)
+    public function showNoticePlus($id): void
     {
         $notice = $this->table[$id];
 
-        $this->out->elementStart('li', array('class' => 'h-entry notice',
-                                             'id' => 'notice-' . $id));
+        $this->out->elementStart('li', ['class' => 'h-entry notice',
+                                        'id'    => 'notice-' . $id]);
 
         $item = $this->newListItem($notice);
         $item->show();
@@ -119,7 +104,7 @@ class ConversationTree extends NoticeList
         if (array_key_exists($id, $this->tree)) {
             $children = $this->tree[$id];
 
-            $this->out->elementStart('ol', array('class' => 'notices threaded-replies xoxo'));
+            $this->out->elementStart('ol', ['class' => 'notices threaded-replies xoxo']);
 
             sort($children);
 
@@ -138,9 +123,9 @@ class ConversationTree extends NoticeList
      *
      * @param Notice $notice Notice to display
      *
-     * @return NoticeListItem a list item to show
+     * @return ConversationTreeItem a list item to show
      */
-    function newListItem(Notice $notice)
+    public function newListItem(Notice $notice): ConversationTreeItem
     {
         return new ConversationTreeItem($notice, $this->out);
     }
