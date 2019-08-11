2a82cfdb2a06e5285d6f282e699b258620318df9
Diogo Cordeiro <diogo@fc.up.pt>
2019-08-11 04:21:26 +0100

    [TagCloud] Add Readme

--- plugins/TagCloud/TagCloudPlugin.php
+++ plugins/TagCloud/TagCloudPlugin.php
@@ -1,16 +1,30 @@
 <?php
+// This file is part of GNU social - https://www.gnu.org/software/social
+//
+// GNU social is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GNU social is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with GNU social.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * GNU social plugin for "tag clouds" in the UI
  *
  * @category  UI
  * @package   GNUsocial
  * @author    Mikael Nordfeldth <mmn@hethane.se>
- * @copyright 2016 Free Software Foundation, Inc.
- * @license   http://www.fsf.org/licensing/licenses/agpl-3.0.html AGPL 3.0
- * @link      http://gnu.io/social/
+ * @copyright 2016 Free Software Foundation, Inc http://www.fsf.org
+ * @license   https://www.gnu.org/licenses/agpl.html GNU AGPL v3 or later
  */
 
-if (!defined('GNUSOCIAL')) { exit(1); }
+defined('GNUSOCIAL') || die();
 
 class TagCloudPlugin extends Plugin {
     const PLUGIN_VERSION = '2.0.0';
@@ -27,8 +41,8 @@ class TagCloudPlugin extends Plugin {
     {
         // TRANS: Menu item in search group navigation panel.
         $menu->out->menuItem(common_local_url('publictagcloud'), _m('MENU','Recent tags'),
-                             // TRANS: Menu item title in search group navigation panel.
-                             _('Recent tags'), $menu->actionName === 'publictagcloud', 'nav_recent-tags');
+            // TRANS: Menu item title in search group navigation panel.
+            _('Recent tags'), $menu->actionName === 'publictagcloud', 'nav_recent-tags');
     }
 
     public function onEndShowSections(Action $action)
@@ -36,20 +50,20 @@ class TagCloudPlugin extends Plugin {
         $cloud = null;
 
         switch (true) {
-        case $action instanceof AllAction:
-            $cloud = new InboxTagCloudSection($action, $action->getTarget());
-            break;
-        case $action instanceof AttachmentAction:
-            $cloud = new AttachmentTagCloudSection($action);
-            break;
-        case $action instanceof PublicAction:
-            $cloud = new PublicTagCloudSection($action);
-            break;
-        case $action instanceof ShowstreamAction:
-            $cloud = new PersonalTagCloudSection($action, $action->getTarget());
-            break;
-        case $action instanceof GroupAction:
-            $cloud = new GroupTagCloudSection($action, $action->getGroup());
+            case $action instanceof AllAction:
+                $cloud = new InboxTagCloudSection($action, $action->getTarget());
+                break;
+            case $action instanceof AttachmentAction:
+                $cloud = new AttachmentTagCloudSection($action);
+                break;
+            case $action instanceof PublicAction:
+                $cloud = new PublicTagCloudSection($action);
+                break;
+            case $action instanceof ShowstreamAction:
+                $cloud = new PersonalTagCloudSection($action, $action->getTarget());
+                break;
+            case $action instanceof GroupAction:
+                $cloud = new GroupTagCloudSection($action, $action->getGroup());
         }
 
         if (!is_null($cloud)) {
@@ -59,13 +73,15 @@ class TagCloudPlugin extends Plugin {
 
     public function onPluginVersion(array &$versions): bool
     {
-        $versions[] = array('name' => 'TagCloud',
-                            'version' => self::PLUGIN_VERSION,
-                            'author' => 'Mikael Nordfeldth',
-                            'homepage' => 'https://gnu.io/social',
-                            'description' =>
-                            // TRANS: Plugin description.
-                            _m('Adds tag clouds to stream pages'));
+        $versions[] = [
+            'name' => 'TagCloud',
+            'version' => self::PLUGIN_VERSION,
+            'author' => 'Mikael Nordfeldth',
+            'homepage' => 'https://gnu.io/social',
+            'description' =>
+            // TRANS: Module description.
+            _m('Adds tag clouds to stream pages')
+        ];
         return true;
     }
 }
