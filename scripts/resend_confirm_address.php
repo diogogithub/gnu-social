44f4c9374d80b4d850b2d3dac7866da587e29fe8
Alexei Sorokin <sor.alexei@meowr.ru>
2019-09-11 09:58:13 +0300

    [DATABASE] Use time intervals in a SQL standard compliant way

--- scripts/resend_confirm_address.php
+++ scripts/resend_confirm_address.php
@@ -1,5 +1,19 @@
 #!/usr/bin/env php
 <?php
+// This file is part of GNU social - https://www.gnu.org/software/social
+//
+// GNU social is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GNU social is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with GNU social.  If not, see <http://www.gnu.org/licenses/>.
 
 define('INSTALLDIR', dirname(__DIR__));
 define('PUBLICDIR', INSTALLDIR . DIRECTORY_SEPARATOR . 'public');
@@ -17,7 +31,7 @@ all lingering confirmations.
 NOTE: You probably want to do something like this to your database first so
 only relatively fresh accounts get resent this:
 
-    DELETE FROM confirm_address WHERE modified < DATE_SUB(NOW(), INTERVAL 1 month);
+    DELETE FROM confirm_address WHERE modified < (CURRENT_TIMESTAMP - INTERVAL '1' MONTH);
 
 Options:
 
