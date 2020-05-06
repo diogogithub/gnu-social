8a86c5940d3860206d4874754cf482696e2b8aea
Hugo Sales <hugo@fc.up.pt>
2020-05-06 16:03:48 +0000

    [TOOLS] Updated install script to also support mariadb

--- docker/social/install.sh
+++ docker/social/install.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
-PGPASSWORD="${POSTGRES_PASSWORD}" psql -ltq -Upostgres -hdb | \
-    cut -d '|' -f1 | grep -wq "${SOCIAL_DB}"
+case "${DBMS}" in
+    'postgres')
+        PGPASSWORD="${POSTGRES_PASSWORD}" psql -ltq -Upostgres -hdb | \
+            cut -d '|' -f1 | grep -wq "${SOCIAL_DB}"
+        DB_EXISTS=$?
+        ;;
+    'mariadb')
+        mysqlcheck -cqs -uroot -p${MYSQL_ROOT_PASSWORD} -hdb social 2> /dev/null
+        DB_EXISTS=$?
+        exit 1
+        ;;
+    *)
+        echo "Unknown DBMS"
+        exit 1
+esac
 
-if [ ! $? ]; then
-
-    echo ${SOCIAL_DB}
+if [ ! ${DB_EXISTS} ]; then
 
     echo -e "Installing GNU social\nInstalling composer dependencies"
 
