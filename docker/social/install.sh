3dbcdcdb80f1e9b88c3b72b558de1a2184265426
Hugo Sales <someonewithpc@gmail.com>
2020-05-07 21:49:20 +0000

    [DOCKER][TOOLS] Updated docker environment and social install script

--- docker/social/install.sh
+++ docker/social/install.sh
@@ -5,18 +5,20 @@ case "${DBMS}" in
         PGPASSWORD="${POSTGRES_PASSWORD}" psql -ltq -Upostgres -hdb | \
             cut -d '|' -f1 | grep -wq "${SOCIAL_DB}"
         DB_EXISTS=$?
+        DB_TYPE='pgsql'
+        SOCIAL_USER=postgres
         ;;
     'mariadb')
         mysqlcheck -cqs -uroot -p${MYSQL_ROOT_PASSWORD} -hdb social 2> /dev/null
         DB_EXISTS=$?
-        exit 1
+        DB_TYPE='mysql'
         ;;
     *)
         echo "Unknown DBMS"
         exit 1
 esac
 
-if [ ! ${DB_EXISTS} ]; then
+if [ ! ${DB_EXISTS} -o ! -e /var/www/social/config.php ]; then
 
     echo -e "Installing GNU social\nInstalling composer dependencies"
 
@@ -27,7 +29,10 @@ if [ ! ${DB_EXISTS} ]; then
     chmod g+w -R .
     chown -R :www-data .
 
-    php bin/console doctrine:database:create || exit 1
+    php /var/www/social/scripts/install_cli.php --dbtype="${DB_TYPE}" --server="${SOCIAL_DOMAIN}" --sitename="${SOCIAL_SITENAME}" \
+        --host=db --fancy=yes --database="${SOCIAL_DB}" \
+        --username="${SOCIAL_USER}" --password="${SOCIAL_PASSWORD}" \
+        --admin-nick="${SOCIAL_ADMIN_NICK}" --admin-pass="${SOCIAL_ADMIN_PASSWORD}" || exit 1
 
     echo "GNU social is installed"
 fi
