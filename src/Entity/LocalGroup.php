289e6333a274b44c1edd31f850ab8d26dbcd4983
Hugo Sales <hugo@fc.up.pt>
2020-07-21 21:31:19 +0000

    [DATABASE] Re-import the local_group table, as it'll be used as per the new group semantics

---
+++ src/Entity/LocalGroup.php
@@ -0,0 +1,113 @@
+<?php
+
+// {{{ License
+// This file is part of GNU social - https://www.gnu.org/software/soci
+//
+// GNU social is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as publ
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GNU social is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public Li
+// along with GNU social.  If not, see <http://www.gnu.org/licenses/>.
+// }}}
+
+namespace App\Entity;
+
+use DateTimeInterface;
+
+/**
+ * Entity for local groups
+ *
+ * @category  DB
+ * @package   GNUsocial
+ *
+ * @author    Zach Copley <zach@status.net>
+ * @copyright 2010 StatusNet Inc.
+ * @author    Mikael Nordfeldth <mmn@hethane.se>
+ * @copyright 2009-2014 Free Software Foundation, Inc http://www.fsf.org
+ * @author    Hugo Sales <hugo@fc.up.pt>
+ * @copyright 2020 Free Software Foundation, Inc http://www.fsf.org
+ * @license   https://www.gnu.org/licenses/agpl.html GNU AGPL v3 or later
+ */
+class LocalGroup
+{
+    // {{{ Autocode
+
+    private int $group_id;
+    private ?string $nickname;
+    private DateTimeInterface $created;
+    private DateTimeInterface $modified;
+
+    public function setGroupId(int $group_id): self
+    {
+        $this->group_id = $group_id;
+        return $this;
+    }
+
+    public function getGroupId(): int
+    {
+        return $this->group_id;
+    }
+
+    public function setNickname(?string $nickname): self
+    {
+        $this->nickname = $nickname;
+        return $this;
+    }
+
+    public function getNickname(): ?string
+    {
+        return $this->nickname;
+    }
+
+    public function setCreated(DateTimeInterface $created): self
+    {
+        $this->created = $created;
+        return $this;
+    }
+
+    public function getCreated(): DateTimeInterface
+    {
+        return $this->created;
+    }
+
+    public function setModified(DateTimeInterface $modified): self
+    {
+        $this->modified = $modified;
+        return $this;
+    }
+
+    public function getModified(): DateTimeInterface
+    {
+        return $this->modified;
+    }
+
+    // }}} Autocode
+
+    public static function schemaDef(): array
+    {
+        return [
+            'name'        => 'local_group',
+            'description' => 'Record for a user group on the local site, with some additional info not in user_group',
+            'fields'      => [
+                'group_id' => ['type' => 'int', 'not null' => true, 'description' => 'group represented'],
+                'nickname' => ['type' => 'varchar', 'length' => 64, 'description' => 'group represented'],
+                'created'  => ['type' => 'datetime', 'not null' => true, 'default' => '0000-00-00 00:00:00', 'description' => 'date this record was created'],
+                'modified' => ['type' => 'datetime', 'not null' => true, 'default' => 'CURRENT_TIMESTAMP', 'description' => 'date this record was modified'],
+            ],
+            'primary key'  => ['group_id'],
+            'foreign keys' => [
+                'local_group_group_id_fkey' => ['user_group', ['group_id' => 'id']],
+            ],
+            'unique keys' => [
+                'local_group_nickname_key' => ['nickname'],
+            ],
+        ];
+    }
+}
