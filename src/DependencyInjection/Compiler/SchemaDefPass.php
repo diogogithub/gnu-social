74d5085d5f84fd7f6f4a4d6fcd1a2f1313040e23
Hugo Sales <hugo@fc.up.pt>
2020-08-03 21:08:47 +0000

    [FORMATTING] Update license header in SchemaDef compiler pass

--- src/DependencyInjection/Compiler/SchemaDefPass.php
+++ src/DependencyInjection/Compiler/SchemaDefPass.php
@@ -1,21 +1,21 @@
 <?php
 
-/*
- * This file is part of GNU social - https://www.gnu.org/software/social
- *
- * GNU social is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * GNU social is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with GNU social.  If not, see <http://www.gnu.org/licenses/>.
- */
+// {{{ License
+// This file is part of GNU social - https://www.gnu.org/software/social
+//
+// GNU social is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GNU social is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with GNU social.  If not, see <http://www.gnu.org/licenses/>.
+// }}}
 
 /**
  * Compiler pass which triggers Symfony to tell Doctrine to
@@ -36,7 +36,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
 /**
- * Register a new ORM driver to allow use to use the old schemaDef format
+ * Register a new ORM driver to allow use to use the old (and better) schemaDef format
  */
 class SchemaDefPass implements CompilerPassInterface
 {
