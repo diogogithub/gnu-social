113c250c41e77084045f08db7a28fbf3c00921e2
Hugo Sales <hugo@fc.up.pt>
2020-08-07 02:05:28 +0000

    [MEDIA][AVATAR] Handle avatar validation and storage

---
+++ components/Media/Media.php
@@ -0,0 +1,45 @@
+<?php
+
+// {{{ License
+// This file is part of GNU social - https://www.gnu.org/software/social
+//
+// GNU social is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GNU social is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with GNU social.  If not, see <http://www.gnu.org/licenses/>.
+// }}}
+
+namespace Component\Media;
+
+use App\Core\Module;
+use App\Entity\File;
+use Symfony\Component\HttpFoundation\File\File as SymfonyFile;
+
+class Media extends Module
+{
+    public static function validateAndStoreFile(SymfonyFile $sfile, string $dest_dir, ?string $title = null, bool $is_local = true): File
+    {
+        // The following properly gets the mimetype with `file` or other
+        // available methods, so should be safe
+        $hash = hash_file(File::FILEHASH_ALGO, $sfile->getPathname());
+        $file = File::create([
+            'file_hash' => $hash,
+            'mimetype'  => $sfile->getMimeType(),
+            'size'      => $sfile->getSize(),
+            'title'     => $title,
+            'timestamp' => $sfile->getMTime(),
+            'is_local'  => $is_local,
+        ]);
+        $sfile->move($dest_dir, $hash);
+        // TODO Normalize file types
+        return $file;
+    }
+}
