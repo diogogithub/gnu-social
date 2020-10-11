diogogithub/gnu-social

commit 9a515b9234eeb2e500e88899fb31f64535b618cc

[SCHEMA] Improve timestamp storage
Avoid the use of deprecated MariaDB "zero dates" globally. If they're present
as attribute defaults somewhere, they will be replaced with NULL implicitly.
The existing "zero dates" in MariaDB storage will be left intact and this
should not present any issues.

The "timestamp" type in table definitions now corresponds to DATETIME in
MariaDB with "DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP", which
should be close enough to the original behaviour for compatibility purposes.
It is now the recommended type for "modified" attributes, because of the
update trigger on MariaDB. But there is no such trigger implemented on
PostgreSQL as of this moment.

classes/Oauth_application.php
@@ -1,8 +1,24 @@
 <?php
+// This file is part of GNU social - https://www.gnu.org/software/social
+//
+// GNU social is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GNU social is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with GNU social.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * Table Definition for oauth_application
  */
-require_once INSTALLDIR.'/classes/Memcached_DataObject.php';
+
+defined('GNUSOCIAL') || die();
 
 class Oauth_application extends Managed_DataObject
 {
@@ -22,8 +38,8 @@ class Oauth_application extends Managed_DataObject
     public $callback_url;                    // varchar(191)   not_null   not 255 because utf8mb4 takes more space
     public $type;                            // tinyint(1)
     public $access_type;                     // tinyint(1)
-    public $created;                         // datetime()   not_null default_0000-00-00%2000%3A00%3A00
-    public $modified;                        // datetime()   not_null default_CURRENT_TIMESTAMP
+    public $created;                         // datetime()
+    public $modified;                        // timestamp()   not_null default_CURRENT_TIMESTAMP
 
     /* the code above is auto generated do not remove the tag below */
     ###END_AUTOCODE
@@ -35,12 +51,12 @@ class Oauth_application extends Managed_DataObject
     public static $browser = 1;
     public static $desktop = 2;
 
-    function getConsumer()
+    public function getConsumer()
     {
         return Consumer::getKV('consumer_key', $this->consumer_key);
     }
 
-    static function maxDesc()
+    public static function maxDesc()
     {
         // This used to default to textlimit or allow unlimited descriptions,
         // but this isn't part of a notice and the field's limited to 191 chars
@@ -57,13 +73,13 @@ class Oauth_application extends Managed_DataObject
         }
     }
 
-    static function descriptionTooLong($desc)
+    public static function descriptionTooLong($desc)
     {
         $desclimit = self::maxDesc();
         return ($desclimit > 0 && !empty($desc) && (mb_strlen($desc) > $desclimit));
     }
 
-    function setAccessFlags($read, $write)
+    public function setAccessFlags($read, $write)
     {
         if ($read) {
             $this->access_type |= self::$readAccess;
@@ -78,7 +94,7 @@ class Oauth_application extends Managed_DataObject
         }
     }
 
-    function setOriginal($filename)
+    public function setOriginal($filename)
     {
         $imagefile = new ImageFile(null, Avatar::path($filename));
 
@@ -91,7 +107,7 @@ class Oauth_application extends Managed_DataObject
         return $this->update($orig);
     }
 
-    static function getByConsumerKey($key)
+    public static function getByConsumerKey($key)
     {
         if (empty($key)) {
             return null;
@@ -113,11 +129,9 @@ class Oauth_application extends Managed_DataObject
      *
      * @return void
      */
-    function uploadLogo()
+    public function uploadLogo()
     {
-        if ($_FILES['app_icon']['error'] ==
-            UPLOAD_ERR_OK) {
-
+        if ($_FILES['app_icon']['error'] == UPLOAD_ERR_OK) {
             try {
                 $imagefile = ImageFile::fromUpload('app_icon');
             } catch (Exception $e) {
@@ -126,10 +140,12 @@ class Oauth_application extends Managed_DataObject
                 return;
             }
 
-            $filename = Avatar::filename($this->id,
-                                         image_type_to_extension($imagefile->type),
-                                         null,
-                                         'oauth-app-icon-'.common_timestamp());
+            $filename = Avatar::filename(
+                $this->id,
+                image_type_to_extension($imagefile->type),
+                null,
+                'oauth-app-icon-' . common_timestamp()
+            );
 
             $filepath = Avatar::path($filename);
 
@@ -139,9 +155,9 @@ class Oauth_application extends Managed_DataObject
         }
     }
 
-    function delete($useWhere=false)
+    public function delete($useWhere = false)
     {
-        $this->_deleteAppUsers();
+        $this->deleteAppUsers();
 
         $consumer = $this->getConsumer();
         $consumer->delete();
@@ -149,7 +165,7 @@ class Oauth_application extends Managed_DataObject
         return parent::delete($useWhere);
     }
 
-    function _deleteAppUsers()
+    private function deleteAppUsers()
     {
         $oauser = new Oauth_application_user();
         $oauser->application_id = $this->id;
@@ -173,8 +189,8 @@ class Oauth_application extends Managed_DataObject
                 'callback_url' => array('type' => 'varchar', 'length' => 191, 'description' => 'url to redirect to after authentication'),
                 'type' => array('type' => 'int', 'size' => 'tiny', 'default' => 0, 'description' => 'type of app, 1 = browser, 2 = desktop'),
                 'access_type' => array('type' => 'int', 'size' => 'tiny', 'default' => 0, 'description' => 'default access type, bit 1 = read, bit 2 = write'),
-                'created' => array('type' => 'datetime', 'not null' => true, 'default' => '0000-00-00 00:00:00', 'description' => 'date this record was created'),
-                'modified' => array('type' => 'datetime', 'not null' => true, 'default' => 'CURRENT_TIMESTAMP', 'description' => 'date this record was modified'),
+                'created' => array('type' => 'datetime', 'description' => 'date this record was created'),
+                'modified' => array('type' => 'datetime', 'not null' => true, 'description' => 'date this record was modified'),
             ),
             'primary key' => array('id'),
             'unique keys' => array(

lib/database/mysqlschema.php
@@ -145,8 +145,12 @@ class MysqlSchema extends Schema
                 if (preg_match('/(^|\s)auto_increment(\s|$)/i', $extra)) {
                     $field['auto_increment'] = true;
                 }
-                // $row['EXTRA'] may contain 'on update CURRENT_TIMESTAMP'
-                // ^ ...... how to specify?
+                if (preg_match(
+                    '/(^|\s)on update CURRENT_TIMESTAMP(\(\))?(\s|$)/i',
+                    $extra
+                )) {
+                    $field['auto_update_timestamp'] = true;
+                }
             }
 
             $table_props = $this->getTableProperties($table, ['TABLE_COLLATION']);
@@ -457,13 +461,17 @@ class MysqlSchema extends Schema
         $line = [];
         $line[] = parent::columnSql($name, $cd);
 
-        // This'll have been added from our transform of 'serial' type
+        // This'll have been added from our transform of "serial" type
         if (!empty($cd['auto_increment'])) {
-            $line[] = 'auto_increment';
+            $line[] = 'AUTO_INCREMENT';
+        }
+        // This'll have been added from our transform of "timestamp" type
+        if (!empty($cd['auto_update_timestamp'])) {
+            $line[] = 'ON UPDATE CURRENT_TIMESTAMP';
         }
 
         if (!empty($cd['description'])) {
-            $line[] = 'comment';
+            $line[] = 'COMMENT';
             $line[] = $this->quoteValue($cd['description']);
         }
 

lib/database/pgsqlschema.php
@@ -413,24 +413,9 @@ class PgsqlSchema extends Schema
             // No convenient support for field descriptions
             unset($col['description']);
 
-            switch ($col['type']) {
-                case 'serial':
-                    $col['type'] = 'int';
-                    $col['auto_increment'] = true;
-                    break;
-                case 'timestamp':
-                    // FIXME: ON UPDATE CURRENT_TIMESTAMP
-                    if (!array_key_exists('default', $col)) {
-                        $col['default'] = 'CURRENT_TIMESTAMP';
-                    }
-                    // no break
-                case 'datetime':
-                    // Replace archaic MySQL-specific zero dates with NULL
-                    if (($col['default'] ?? null) === '0000-00-00 00:00:00') {
-                        $col['default'] = null;
-                        $col['not null'] = false;
-                    }
-                    break;
+            if ($col['type'] === 'serial') {
+                $col['type'] = 'int';
+                $col['auto_increment'] = true;
             }
 
             $col['type'] = $this->mapType($col);

lib/database/schema.php
@@ -999,6 +999,23 @@ class Schema
     public function filterDef(string $tableName, array $tableDef)
     {
         foreach ($tableDef['fields'] as $name => &$col) {
+            switch ($col['type']) {
+                case 'timestamp':
+                    $col['type'] = 'datetime';
+                    if (!array_key_exists('default', $col)) {
+                        $col['default'] = 'CURRENT_TIMESTAMP';
+                        // FIXME: PostgreSQL support.
+                        $col['auto_update_timestamp'] = true;
+                    }
+                    // no break
+                case 'datetime':
+                    // Replace archaic MariaDB-specific "zero dates" with NULL
+                    if (($col['default'] ?? null) === '0000-00-00 00:00:00') {
+                        $col['default'] = null;
+                        $col['not null'] = false;
+                    }
+                    break;
+            }
             if (array_key_exists('default', $col) && is_null($col['default'])) {
                 unset($col['default']);
             }

plugins/EmailSummary/classes/Email_summary_status.php
@@ -20,13 +20,11 @@
  * @category  Data
  * @package   GNUsocial
  * @author    Evan Prodromou <evan@status.net>
- * @copyright 2010, StatusNet, Inc.
+ * @copyright 2010 StatusNet, Inc.
  * @license   https://www.gnu.org/licenses/agpl.html GNU AGPL v3 or later
  */
 
 defined('GNUSOCIAL') || die();
-
-require_once INSTALLDIR . '/classes/Memcached_DataObject.php';
 
 /**
  * Data class for email summaries
@@ -34,7 +32,7 @@ require_once INSTALLDIR . '/classes/Memcached_DataObject.php';
  * Email summary information for users
  *
  * @category  Action
- * @copyright 2010, StatusNet, Inc.
+ * @copyright 2010 StatusNet, Inc.
  * @license   https://www.gnu.org/licenses/agpl.html GNU AGPL v3 or later
  *
  * @see       DB_DataObject
@@ -55,7 +53,7 @@ class Email_summary_status extends Managed_DataObject
                 'user_id' => array('type' => 'int', 'not null' => true, 'description' => 'user id'),
                 'send_summary' => array('type' => 'bool', 'default' => true, 'not null' => true, 'description' => 'whether to send a summary or not'),
                 'last_summary_id' => array('type' => 'int', 'description' => 'last summary id'),
-                'created' => array('type' => 'datetime', 'not null' => true, 'description' => 'date this record was created'),
+                'created' => array('type' => 'datetime', 'description' => 'date this record was created'),
                 'modified' => array('type' => 'timestamp', 'not null' => true, 'description' => 'date this record was modified'),
             ),
             'primary key' => array('user_id'),

commit 06dfd91a82dae81fb500eaed5112d4753b951dab

Various fixes
Fix OAuth and Realtime issues introduced in 9a515b9234eeb2e500e88899fb31f64535b618cc

[DATABASE] Fix an empty default value mistake introduced in
fde929b151f2b286cb60d226c47570209199c9c5

[DATABASE][PostgreSQL] Avoid use of pg_constraint.consrc, which was removed in
PostgreSQL 12.

[DATABASE][MariaDB] Fix a typo introduced in aed2344bd4c512dae48d1a5bd2eda218f914ae11

[DAEMON] Wrap an assignment inside "switch":
a follow-up to adc689cb15d611c83ebb9c5aaeaa1008ec14da28

classes/Oauth_application.php
@@ -190,7 +190,7 @@ class Oauth_application extends Managed_DataObject
                 'type' => array('type' => 'int', 'size' => 'tiny', 'default' => 0, 'description' => 'type of app, 1 = browser, 2 = desktop'),
                 'access_type' => array('type' => 'int', 'size' => 'tiny', 'default' => 0, 'description' => 'default access type, bit 1 = read, bit 2 = write'),
                 'created' => array('type' => 'datetime', 'description' => 'date this record was created'),
-                'modified' => array('type' => 'datetime', 'not null' => true, 'description' => 'date this record was modified'),
+                'modified' => array('type' => 'timestamp', 'not null' => true, 'description' => 'date this record was modified'),
             ),
             'primary key' => array('id'),
             'unique keys' => array(

lib/database/mysqlschema.php
@@ -461,7 +461,7 @@ class MysqlSchema extends Schema
             $phrase[] = "ENGINE '{$engine}'";
         }
         if (strtolower($oldProps['TABLE_COLLATION']) !== "{$charset}_bin") {
-            $phrase[] = "CONVERT TO CHARACTER SET '{$charset} COLLATE '{$charset}_bin'";
+            $phrase[] = "CONVERT TO CHARACTER SET '{$charset}' COLLATE '{$charset}_bin'";
             $phrase[] = "DEFAULT CHARACTER SET '{$charset}'";
             $phrase[] = "DEFAULT COLLATE '{$charset}_bin'";
         }

lib/database/pgsqlschema.php
@@ -328,7 +328,8 @@ class PgsqlSchema extends Schema
     {
         $data = $this->fetchQueryData(
             <<<END
-            SELECT "cols"."attname" AS "col", "con"."consrc" AS "check"
+            SELECT "cols"."attname" AS "col",
+                pg_get_expr("con"."conbin", "con"."conrelid") AS "check"
               FROM pg_constraint AS "con"
               INNER JOIN pg_attribute AS "cols"
               ON "con"."conrelid" = "cols"."attrelid"

lib/database/schema.php
@@ -878,7 +878,7 @@ class Schema
     {
         // MDB2_Driver_Common::quote changes empty strings to "NULL".
         if ($val === '') {
-            return '';
+            return "''";
         }
         return $this->conn->quote($val);
     }

lib/modules/Realtime/classes/Realtime_channel.php
@@ -85,7 +85,7 @@ class Realtime_channel extends Managed_DataObject
                 'created' => ['type' => 'datetime',
                     'not null' => true,
                     'description' => 'date this record was created'],
-                'modified' => ['type' => 'datetime',
+                'modified' => ['type' => 'timestamp',
                     'not null' => true,
                     'description' => 'date this record was modified'],
             ],

lib/util/daemon.php
@@ -78,12 +78,12 @@ class Daemon
 
     public function background()
     {
-        // Database connection will likely get lost after forking
+        // Database connection will be likely lost after forking.
         $this->resetDb();
 
         // Double-forking.
         foreach (['single', 'double'] as $v) {
-            switch ($pid = pcntl_fork()) {
+            switch (($pid = pcntl_fork())) {
                 case -1: // error
                     common_log(LOG_ERR, 'Could not fork.');
                     return false;

plugins/EmailSummary/classes/Email_summary_status.php
@@ -44,7 +44,7 @@ class Email_summary_status extends Managed_DataObject
     public $send_summary;                    // bool    not_null default_true
     public $last_summary_id;                 // int(4)  null
     public $created;                         // datetime not_null
-    public $modified;                        // datetime not_null
+    public $modified;                        // timestamp not_null
 
     public static function schemaDef()
     {

plugins/Poll/classes/User_poll_prefs.php
@@ -41,7 +41,7 @@ class User_poll_prefs extends Managed_DataObject
     public $user_id;          // int id
     public $hide_responses;   // bool
     public $created;          // datetime
-    public $modified;         // datetime
+    public $modified;         // timestamp
 
     /**
      * The One True Thingy that must be defined and declared.