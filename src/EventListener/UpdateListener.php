fbc85086fd1785849be224fe1693902906dc4185
Diogo Machado <diogo.smac@gmail.com>
2020-10-13 23:45:48 +0100

    [DB][TESTS] Implement Doctrine event listener to update timestamps on modification, and related tests

---
+++ src/EventListener/UpdateListener.php
@@ -0,0 +1,44 @@
+<?php
+
+// {{{ License
+// This file is part of GNU social - https://www.gnu.org/software/social
+//
+// GNU social is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GNU social is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with GNU social.  If not, see <http://www.gnu.org/licenses/>.
+// }}}
+
+namespace App\EventListener;
+
+use DateTime;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+
+class UpdateListener
+{
+    public function preUpdate(PreUpdateEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        if (!method_exists($entity, 'setModified')) {
+            return false;
+        }
+
+        $em  = $args->getEntityManager();
+        $uow = $em->getUnitOfWork();
+        $md  = $em->getClassMetadata(get_class($entity));
+
+        $entity->setModified(new DateTime());
+        $uow->recomputeSingleEntityChangeSet($md, $entity);
+
+        return true;
+    }
+}
