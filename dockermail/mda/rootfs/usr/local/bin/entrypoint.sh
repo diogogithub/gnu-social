ea557d6fc7d8e2308a17033ffb83a622d5120f4a
Pastilhas <up201704982@fe.up.pt>
2020-10-20 22:11:54 +0100

    [MAILSERVER] Setup docker mail server

---
+++ dockermail/mda/rootfs/usr/local/bin/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if ! [ -r /etc/dovecot/dh.pem.created ]
+then
+  echo "Using pre-generated Diffie Hellman parameters until the new one is generated."
+  /usr/local/bin/dh.sh &
+fi
+
+rm -f /run/dovecot/master.pid
+
+dockerize \
+  -template /etc/dovecot/conf.d/10-master.conf.templ:/etc/dovecot/conf.d/10-master.conf \
+  -template /etc/dovecot/conf.d/15-lda.conf.templ:/etc/dovecot/conf.d/15-lda.conf \
+  -template /etc/dovecot/conf.d/20-submission.conf.templ:/etc/dovecot/conf.d/20-submission.conf \
+  -template /etc/dovecot/dovecot-sql.conf.ext.templ:/etc/dovecot/dovecot-sql.conf.ext \
+  -wait tcp://${MYSQL_HOST}:3306 \
+  -wait file://${SSL_CERT} \
+  -wait file://${SSL_KEY} \
+  -timeout ${WAITSTART_TIMEOUT} \
+  /usr/sbin/dovecot -F
