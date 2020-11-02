7a41cb57e026feacca96e48fa900e2d210febc28
Pastilhas <up201704982@fe.up.pt>
2020-11-02 20:04:16 +0000

    [MAILSERVER] Fixed small bug with ssl certificates

--- docker/mail/rootfs/usr/bin/start.sh
+++ docker/mail/rootfs/usr/bin/start.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
+CERTBOT="/etc/letsencrypt/live/$domain/fullchain.pem"
+KEYBOT="/etc/letsencrypt/live/$domain/privkey.pem"
+
 # Config postfix
 postconf -e myhostname="$MAILNAME"
 postconf -e mydomain="$DOMAINNAME"
-postconf -e smtpd_tls_cert_file="$SSL_CERT"
-postconf -e smtpd_tls_key_file="$SSL_KEY"
+postconf -e smtpd_tls_cert_file="$CERTBOT"
+postconf -e smtpd_tls_key_file="$KEYBOT"
 
 # Config dovecot
-sed -i -e "s#^\s*ssl_cert\s*=.*#ssl_cert = $SSL_CERT#" /etc/dovecot/dovecot.conf
-sed -i -e "s#^\s*ssl_key\s*=.*#ssl_key = $SSL_KEY#" /etc/dovecot/dovecot.conf
+sed -i -e "s#^\s*ssl_cert\s*=.*#ssl_cert = <$CERTBOT#" /etc/dovecot/dovecot.conf
+sed -i -e "s#^\s*ssl_key\s*=.*#ssl_key = <$KEYBOT#" /etc/dovecot/dovecot.conf
 sed -i -e "s#^\s*hostname\s*=.*#hostname = $MAILNAME#" /etc/dovecot/dovecot.conf
 sed -i -e "s#^\s*postmaster_address\s*=.*#postmaster_address = $POSTMASTER#" /etc/dovecot/dovecot.conf
 
@@ -16,15 +19,14 @@ sed -i -e "s#^\s*postmaster_address\s*=.*#postmaster_address = $POSTMASTER#" /et
 sed -i -e "s/#HOSTNAME/$MAILNAME/" /etc/opendkim/TrustedHosts
 
 # Run openssl
-if [ ! -e /etc/ssl/.ssl-generated ]
+if [ $signed -eq 0 ]
 then
-	openssl genrsa -des3 -passout pass:asdf -out /etc/ssl/mail.pass.key 2048 && \
-	openssl rsa -passin pass:asdf -in /etc/ssl/mail.pass.key -out "$SSL_KEY"
-	rm /etc/ssl/mail.pass.key
-	openssl req -new -key "$SSL_KEY" -out /etc/ssl/mail.csr \
-	  -subj "/C=UK/ST=England/L=London/O=OrgName/OU=IT Department/CN=$MAILNAME"
-	openssl x509 -req -days 365 -in /etc/ssl/mail.csr -signkey "$SSL_KEY" -out "$SSL_CERT"
-	echo "Do not remove this file." >> /etc/ssl/.ssl-generated
+	openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout "$SSL_KEY" -out "$SSL_CERT" \
+		-subj "/C=UK/ST=England/L=London/O=OrgName/OU=IT Department/CN=$MAILNAME"
+	postconf -e smtpd_tls_cert_file="$SSL_CERT"
+	postconf -e smtpd_tls_key_file="$SSL_KEY"
+	sed -i -e "s#^\s*ssl_cert\s*=.*#ssl_cert = <$SSL_CERT#" /etc/dovecot/dovecot.conf
+	sed -i -e "s#^\s*ssl_key\s*=.*#ssl_key = <$SSL_KEY#" /etc/dovecot/dovecot.conf
 fi
 
 # Run opendkim
