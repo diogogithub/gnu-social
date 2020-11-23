ac3602f26ebdaaa49a72fe47e06012258723e049
Pastilhas <up201704982@fe.up.pt>
2020-11-23 14:55:33 +0000

    [DOCKER][MAIL] Cleanup opendkim.conf Also improved consistency in other files

--- docker/mail/rootfs/usr/bin/start.sh
+++ docker/mail/rootfs/usr/bin/start.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
 # Run openssl
-if [ ! -e "$SSL_CERT" ]
+if [ ! -e "${SSL_CERT}" ]
 then
-	mkdir -p "$(dirname $SSL_CERT)" "$(dirname $SSL_KEY)"
-	openssl req -x509 -nodes -newkey rsa:2018 -days 365 -keyout "$SSL_CERT" -out "$SSL_KEY"
+	mkdir -p "$(dirname ${SSL_CERT})" "$(dirname $SSL_KEY)"
+	openssl req -x509 -nodes -newkey rsa:2018 -days 365 -keyout "${SSL_CERT}" -out "${SSL_KEY}"
 fi
 
 # Run opendkim
 if [ ! -e "/var/opendkim/keys/default.private" ]
 then
-	mkdir -p /var/opendkim/keys
-	opendkim-genkey -d "$DOMAINNAME" -D "/var/opendkim/keys"
+	opendkim-genkey -d "${DOMAINNAME}" -D "/var/opendkim/keys/"
 fi
 
 postmap /etc/mail/aliases /etc/mail/domains /etc/mail/mailboxes /etc/mail/passwd
