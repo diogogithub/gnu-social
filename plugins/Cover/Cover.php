92777db3b295829959c2fdd65110ea9060c3140a
Daniel <daniel.fb25@gmail.com>
2020-11-24 16:47:07 +0000

    [Cover] Started implementing Cover plugin: base class, route, base templates, added tabs in profile template

---
+++ plugins/Cover/Cover.php
@@ -0,0 +1,75 @@
+<?php
+// {{{ License
+
+// This file is part of GNU social - https://www.gnu.org/software/social
+//
+// GNU social is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GNU social is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with GNU social.  If not, see <http://www.gnu.org/licenses/>.
+
+// }}}
+
+namespace Plugin\Cover;
+
+use App\Core\Event;
+use App\Core\Module;
+use App\Core\Router\RouteLoader;
+
+class Cover extends Module
+{
+    /**
+     * Map URLs to actions
+     *
+     * @param RouteLoader $r
+     *
+     * @return bool hook value; true means continue processing, false means stop.
+     */
+    public function onAddRoute(RouteLoader $r): bool
+    {
+        $r->connect('settings_cover', 'settings/cover', [Controller\Cover::class, 'cover']);
+
+        return Event::next;
+    }
+
+    /**
+     * Populate twig vars
+     *
+     * @param array $vars
+     *
+     * @return bool hook value; true means continue processing, false means stop.
+     */
+    public function onStartTwigPopulateVars(array &$vars): bool
+    {
+        /*
+        $vars['tabs'] = [['title' => 'Poll',
+            'href'                => 'newpoll',
+        ]];
+        */
+        $vars['profile_tabs'] = [['title' => 'Cover',
+            'href'                        => 'settings_cover',
+        ]];
+        return Event::next;
+    }
+
+    /**
+     * Output our dedicated stylesheet
+     *
+     * @param array $styles stylesheets path
+     *
+     * @return bool hook value; true means continue processing, false means stop.
+     */
+    public function onStartShowStyles(array &$styles): bool
+    {
+        //$styles[] = 'poll/poll.css';
+        return Event::next;
+    }
+}
