ee87961d9603248b1139490c6f04b0d166437c4a
Hugo Sales <hugo@hsal.es>
2021-04-15 22:47:33 +0000

    [ImageThumbnail] Structure of plugin to generate thumbnails for image attachments

---
+++ plugins/ImageThumbnail/Controller/ImageThumbnail.php
@@ -0,0 +1,53 @@
+<?php
+
+// {{{ License
+
+// This file is part of GNU social - https://www.gnu.org/software/social
+//
+// GNU social is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GNU social is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with GNU social.  If not, see <http://www.gnu.org/licenses/>.
+
+// }}}
+
+namespace Plugin\ImageThumbnail\Controller;
+
+use App\Core\Controller;
+use App\Core\DB\DB;
+use App\Util\Common;
+use Symfony\Component\HttpFoundation\Request;
+
+class ImageThumbnail extends Controller
+{
+    /**
+     * Get a thumbnail for the attachment with id $id
+     */
+    public function thumbnail(Request $request, int $id)
+    {
+        $attachemnt = DB::findOneBy('attachment', ['id' => $id]);
+        if (!is_null($attachemnt->getScope())) {
+            // && ($attachemnt->scope | VisibilityScope::PUBLIC) != 0
+            // $user = Common::ensureLoggedIn();
+            assert(false, 'Attachment scope not implemented');
+        }
+
+        // TODO rate limit
+
+        $max_width  = Common::config('thumbnail', 'width');
+        $max_height = Common::config('thumbnail', 'height');
+        $width      = Common::clamp($this->int('w') ?? $max_width,  min: 0, max: $max_width);
+        $height     = Common::clamp($this->int('h') ?? $max_height, min: 0, max: $max_height);
+        $crop       = $this->bool('c') ?? false;
+
+        dd($width, $height, $crop);
+    }
+}
