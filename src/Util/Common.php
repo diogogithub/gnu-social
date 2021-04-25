94edad43d9667d42883e5ff7087d09ed970af099
Hugo Sales <hugo@hsal.es>
2021-04-25 21:17:01 +0000

    [UTIL] Add method to validate url

--- src/Util/Common.php
+++ src/Util/Common.php
@@ -235,4 +235,19 @@ abstract class Common
     {
         return min(max($value, $min), $max);
     }
+
+    /**
+     * If $secure is true, only allow https URLs to pass
+     */
+    public function isValidHttpUrl(string $url, bool $ensure_secure = false)
+    {
+        if (empty($url)) {
+            return false;
+        }
+
+        // (if false, we use '?' in 'https?' to say the 's' is optional)
+        $regex = $secure ? '/^https$/' : '/^https?$/';
+        return filter_var($url, FILTER_VALIDATE_URL)
+            && preg_match($regex, parse_url($url, PHP_URL_SCHEME));
+    }
 }
