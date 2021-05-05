f2727f9327bcf541eee6630c86e3b1a36b4f9dbd
Hugo Sales <hugo@hsal.es>
2021-05-05 12:17:16 +0000

    [DEPENDENCIES] Add doctrine/doctrine-fixtures-bundle, which allows populating the database in the testing environment

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Misd\PhoneNumberBundle\MisdPhoneNumberBundle::class                  => ['all' => true],
     HtmlSanitizer\Bundle\HtmlSanitizerBundle::class                      => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class         => ['dev' => true, 'test' => true],
 ];
