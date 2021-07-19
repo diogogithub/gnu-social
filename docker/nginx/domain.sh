c33a65c45e234fb89a92b1ab6b521b1f0556a48d
Eliseu Amaro <mail@eliseuama.ro>
2021-07-19 14:43:49 +0100

    [DOCKER][nginx] Removing default nginx config through docker/nginx/domain.sh. The default config conflicts with 'localhost' server_name.

--- docker/nginx/domain.sh
+++ docker/nginx/domain.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# There's no point in keeping default nginx conf
+# Furthermore, this is an issue when a developer is using localhost as the instance domain
+rm /etc/nginx/conf.d/default.conf
+
 # Can't do sed inplace, because the file would be busy
 cat /var/nginx/social.conf | \
     sed -r "s/%hostname%/${WEB_DOMAIN}/g;" > \
