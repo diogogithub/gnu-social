855d42744230f319689ed8700726e18ed0ca576e
Hugo Sales <hugo@hsal.es>
2021-08-03 10:24:45 +0000

    [UTIL][FORM] Create a utility class that defines common form fields, such as passwords

--- src/Util/FormFields.php
+++ src/Util/FormFields.php
@@ -2,12 +2,15 @@
 
 namespace App\Util;
 
+use function App\Core\I18n\_m;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 abstract class FormFields
 {
-    public static function password()
+    public static function repeated_password()
     {
         return ['password', RepeatedType::class,
             ['type'             => PasswordType::class,
@@ -25,6 +28,19 @@ abstract class FormFields
                 'mapped'          => false,
                 'invalid_message' => _m('The password fields must match'),
             ],
+        ];
+    }
+
+    public static function password()
+    {
+        ['password', PasswordType::class, [
+            'label'       => _m('Password'),
+            'mapped'      => false,
+            'constraints' => [
+                new NotBlank(['message' => _m('Please enter a password')]),
+                new Length(['min' => Common::config('password', 'min_length'), 'minMessage' => _m(['Your password should be at least # characters'], ['count' => Common::config('password', 'min_length')]),
+                    'max'         => Common::config('password', 'max_length'), 'maxMessage' => _m(['Your password should be at most # characters'],  ['count' => Common::config('password', 'max_length')]), ]),
+            ], ],
         ];
     }
 }
