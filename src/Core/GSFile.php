b4a03b814f44a01e21e430dc7bcd6262d117f82c
Diogo Peralta Cordeiro <mail@diogo.site>
2021-08-14 15:05:56 +0100

    [CORE][GSFile] ensureFilenameWithProperExtension: extension isn't an I/O param

--- src/Core/GSFile.php
+++ src/Core/GSFile.php
@@ -231,9 +231,9 @@ class GSFile
      * @param null|string $ext      Extension we believe to be best
      * @param bool        $force    Should we force the extension we believe to be best? Defaults to false
      *
-     * @return null|string
+     * @return null|string the most appropriate filename or null if we deem it imposible
      */
-    public static function ensureFilenameWithProperExtension(string $title, string $mimetype, ?string &$ext = null, bool $force = false): string | null
+    public static function ensureFilenameWithProperExtension(string $title, string $mimetype, ?string $ext = null, bool $force = false): string | null
     {
         $valid_extensions = MimeTypes::getDefault()->getExtensions($mimetype);
 
