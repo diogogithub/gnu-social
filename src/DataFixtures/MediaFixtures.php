6e6d1a946fe5129b256a748977d94d2bbfb91ee7
Hugo Sales <hugo@hsal.es>
2021-08-14 19:49:32 +0000

    [TESTS] Remove MediaFileTest and move setup code to media data fixture

--- src/DataFixtures/MediaFixtures.php
+++ src/DataFixtures/MediaFixtures.php
@@ -21,6 +21,29 @@ class MediaFixtures extends Fixture
             $file = new File($copy_filepath, checkPath: true);
             GSFile::validateAndStoreFileAsAttachment($file, dest_dir: Common::config('attachments', 'dir') . 'test/', title: $title, actor_id: $actor->getId());
         };
+
+        $test_files = [
+            'image.png'         => 'image/png',
+            'image.gif'         => 'image/gif',
+            'image.jpg'         => 'image/jpeg',
+            'image.jpeg'        => 'image/jpeg',
+            'office.pdf'        => 'application/pdf',
+            'wordproc.odt'      => 'application/vnd.oasis.opendocument.text',
+            'wordproc.ott'      => 'application/vnd.oasis.opendocument.text-template',
+            'wordproc.doc'      => 'application/msword',
+            'wordproc.docx'     => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+            'wordproc.rtf'      => 'text/rtf',
+            'spreadsheet.ods'   => 'application/vnd.oasis.opendocument.spreadsheet',
+            'spreadsheet.ots'   => 'application/vnd.oasis.opendocument.spreadsheet-template',
+            'spreadsheet.xls'   => 'application/vnd.ms-excel',
+            'spreadsheet.xlt'   => 'application/vnd.ms-excel',
+            'spreadsheet.xlsx'  => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            'presentation.odp'  => 'application/vnd.oasis.opendocument.presentation',
+            'presentation.otp'  => 'application/vnd.oasis.opendocument.presentation-template',
+            'presentation.ppt'  => 'application/vnd.ms-powerpoint',
+            'presentation.pptx' => 'application/zip', //"application/vnd.openxmlformats-officedocument.presentationml.presentation",
+        ];
+
         $store(INSTALLDIR . '/tests/Media/sample-uploads/image.jpeg', '1x1 JPEG image title');
         $manager->flush();
     }
