e7699b324588d9e50b8d8efec69c6b065c29b5ba
Hugo Sales <hugo@hsal.es>
2021-08-18 17:28:26 +0100

    [TESTS] Raise test coverage for Note to 100%

---
+++ tests/Entity/NoteTest.php
@@ -0,0 +1,64 @@
+<?php
+
+// {{{ License
+// This file is part of GNU social - https://www.gnu.org/software/social
+//
+// GNU social is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GNU social is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with GNU social.  If not, see <http://www.gnu.org/licenses/>.
+// }}}
+
+namespace App\Tests\Entity;
+
+use App\Core\DB\DB;
+use App\Core\VisibilityScope;
+use App\Util\GNUsocialTestCase;
+use Jchook\AssertThrows\AssertThrows;
+
+class NoteTest extends GNUsocialTestCase
+{
+    use AssertThrows;
+
+    public function testGetReplies()
+    {
+        $user    = DB::findOneBy('local_user', ['nickname' => 'taken_user']);
+        $note    = DB::findBy('note', ['gsactor_id' => $user->getId(), 'content' => 'some content', 'reply_to' => null], limit: 1)[0];
+        $replies = $note->getReplies();
+        static::assertSame('some other content', $replies[0]->getContent());
+        static::assertSame($user->getId(), $replies[0]->getGSActorId());
+        static::assertSame($note->getId(), $replies[0]->getReplyTo());
+
+        static::assertSame($user->getNickname(), $replies[0]->getReplyToNickname());
+    }
+
+    public function testIsVisibleTo()
+    {
+        $actor1 = DB::findOneBy('gsactor', ['nickname' => 'taken_user']);
+        $actor2 = DB::findOneBy('gsactor', ['nickname' => 'taken_group']);
+        $actor3 = DB::findOneBy('gsactor', ['nickname' => 'some_user']);
+
+        $note_visible_to_1 = DB::findBy('note', ['gsactor_id' => $actor1->getId(), 'content' => 'private note', 'scope' => VisibilityScope::FOLLOWER], limit: 1)[0];
+        static::assertTrue($note_visible_to_1->isVisibleTo($actor1));
+        static::assertFalse($note_visible_to_1->isVisibleTo($actor2));
+        static::assertFalse($note_visible_to_1->isVisibleTo($actor3));
+
+        $note_public = DB::findBy('note', ['gsactor_id' => $actor1->getId(), 'content' => 'some content', 'reply_to' => null], limit: 1)[0];
+        static::assertTrue($note_public->isVisibleTo($actor1));
+        static::assertTrue($note_public->isVisibleTo($actor2));
+        static::assertTrue($note_public->isVisibleTo($actor3));
+
+        $group_note = DB::findBy('note', ['gsactor_id' => $actor1->getId(), 'content' => 'group note', 'scope' => VisibilityScope::GROUP], limit: 1)[0];
+        static::assertTrue($group_note->isVisibleTo($actor3));
+        static::assertFalse($group_note->isVisibleTo($actor2));
+        static::assertFalse($group_note->isVisibleTo($actor1));
+    }
+}
