someonewithpc/gnusocial

commit c90efe2c52495fb188bd37170c3a9d99259e108c

[UI] Add mechanism for rendering note contents in different formats. Implement plaintext rendering. Use rendered field for note content, rather than the content itself

components/Posting/Posting.php
@@ -25,19 +25,17 @@ use App\Core\Cache;
 use App\Core\DB\DB;
 use App\Core\Event;
 use App\Core\Form;
-use App\Core\GSFile;
 use function App\Core\I18n\_m;
 use App\Core\Modules\Component;
 use App\Entity\Attachment;
-use App\Entity\AttachmentToNote;
-use App\Entity\GSActorToAttachment;
+use App\Entity\GSActor;
 use App\Entity\Note;
 use App\Util\Common;
 use App\Util\Exception\ClientException;
-use App\Util\Exception\DuplicateFoundException;
 use App\Util\Exception\InvalidFormException;
 use App\Util\Exception\RedirectException;
 use App\Util\Exception\ServerException;
+use App\Util\Formatting;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -61,9 +59,8 @@ class Posting extends Component
 
         $actor_id = $user->getId();
         $to_tags  = [];
-        $tags     = Cache::get("actor-circle-{$actor_id}", function () use ($actor_id) {
-            return DB::dql('select c.tag from App\Entity\GSActorCircle c where c.tagger = :tagger', ['tagger' => $actor_id]);
-        });
+        $tags     = Cache::get("actor-circle-{$actor_id}",
+                               fn () => DB::dql('select c.tag from App\Entity\GSActorCircle c where c.tagger = :tagger', ['tagger' => $actor_id]));
         foreach ($tags as $t) {
             $t           = $t['tag'];
             $to_tags[$t] = $t;
@@ -76,8 +73,8 @@ class Posting extends Component
         $initial_content = '';
         Event::handle('PostingInitialContent', [&$initial_content]);
 
-        $content_type = ['Plain Text' => 'text/plain'];
-        Event::handle('PostingAvailableContentTypes', [&$content_type]);
+        $available_content_types = ['Plain Text' => 'text/plain'];
+        Event::handle('PostingAvailableContentTypes', [&$available_content_types]);
 
         $request     = $vars['request'];
         $form_params = [
@@ -86,8 +83,11 @@ class Posting extends Component
             ['visibility',  ChoiceType::class,   ['label' => _m('Visibility:'), 'multiple' => false, 'expanded' => false, 'data' => 'public', 'choices' => [_m('Public') => 'public', _m('Instance') => 'instance', _m('Private') => 'private']]],
             ['to',          ChoiceType::class,   ['label' => _m('To:'), 'multiple' => false, 'expanded' => false, 'choices' => $to_tags]],
         ];
-        if (count($content_type) > 1) {
-            $form_params[] = ['content_type', ChoiceType::class, ['label' => _m('Text format:'), 'multiple' => false, 'expanded' => false, 'data' => 'text/plain', 'choices' => $content_type]];
+        if (count($available_content_types) > 1) {
+            $form_params[] = ['content_type', ChoiceType::class,
+                ['label'      => _m('Text format:'), 'multiple' => false, 'expanded' => false,
+                    'data'    => $available_content_types[array_key_first($available_content_types)],
+                    'choices' => $available_content_types, ], ];
         }
         $form_params[] = ['post_note',   SubmitType::class,   ['label' => _m('Post')]];
         $form          = Form::create($form_params);
@@ -96,7 +96,8 @@ class Posting extends Component
         if ($form->isSubmitted()) {
             $data = $form->getData();
             if ($form->isValid()) {
-                self::storeNote($actor_id, $data['content_type'] ?? array_key_first($content_type), $data['content'], $data['attachments'], is_local: true);
+                $content_type = $data['content_type'] ?? $available_content_types[array_key_first($available_content_types)];
+                self::storeLocalNote($user->getActor(), $data['content'], $content_type, $data['attachments']);
                 throw new RedirectException();
             } else {
                 throw new InvalidFormException();
@@ -108,52 +109,30 @@ class Posting extends Component
         return Event::next;
     }
 
-    /**
-     * Store the given note with $content and $attachments, created by
-     * $actor_id, possibly as a reply to note $reply_to and with flag
-     * $is_local. Sanitizes $content and $attachments
-     *
-     * @throws DuplicateFoundException
-     * @throws ClientException|ServerException
-     */
-    public static function storeNote(int $actor_id, string $content_type, string $content, array $attachments, bool $is_local, ?int $reply_to = null, ?int $repeat_of = null)
+    public static function storeLocalNote(GSActor $actor, string $content, string $content_type, array $attachments, ?Note $reply_to = null, ?Note $repeat_of = null)
     {
+        $rendered = null;
+        Event::handle('RenderNoteContent', [$content, $content_type, &$rendered, $actor, $reply_to]);
         $note = Note::create([
-            'gsactor_id'   => $actor_id,
-            'content_type' => $content_type,
+            'gsactor_id'   => $actor->getId(),
             'content'      => $content,
-            'is_local'     => $is_local,
-            'reply_to'     => $reply_to,
-            'repeat_of'    => $repeat_of,
+            'content_type' => $content_type,
+            'rendered'     => $rendered,
+            'attachments'  => $attachments, // Not a regular field
+            'is_local'     => true,
         ]);
-
-        $processed_attachments = [];
-        foreach ($attachments as $f) { // where $f is a Symfony\Component\HttpFoundation\File\UploadedFile
-            $filesize      = $f->getSize();
-            $max_file_size = Common::config('attachments', 'file_quota');
-            if ($max_file_size < $filesize) {
-                throw new ClientException(_m('No file may be larger than {quota} bytes and the file you sent was {size} bytes. ' .
-                    'Try to upload a smaller version.', ['quota' => $max_file_size, 'size' => $filesize]));
-            }
-            Event::handle('EnforceUserFileQuota', [$filesize, $actor_id]);
-            $processed_attachments[] = [GSFile::sanitizeAndStoreFileAsAttachment($f), $f->getClientOriginalName()];
-        }
-
-        DB::persist($note);
-
-        // Need file and note ids for the next step
+        Event::handle('ProcessNoteContent', [$note->getId(), $content, $content_type]);
         DB::flush();
-        if ($processed_attachments != []) {
-            foreach ($processed_attachments as [$a, $fname]) {
-                if (empty(DB::findBy('gsactor_to_attachment', ['attachment_id' => $a->getId(), 'gsactor_id' => $actor_id]))) {
-                    DB::persist(GSActorToAttachment::create(['attachment_id' => $a->getId(), 'gsactor_id' => $actor_id]));
-                }
-                DB::persist(AttachmentToNote::create(['attachment_id' => $a->getId(), 'note_id' => $note->getId(), 'title' => $fname]));
-            }
-            DB::flush();
-        }
+    }
 
-        Event::handle('ProcessNoteContent', [$note->getId(), $content]);
+    public function onRenderNoteContent(string $content, string $content_type, ?string &$rendered, GSActor $author, ?Note $reply_to = null)
+    {
+        if ($content_type === 'text/plain') {
+            $content  = Formatting::renderPlainText($content);
+            $rendered = Formatting::linkifyMentions($content, $author, $reply_to);
+            return Event::stop;
+        }
+        return Event::next;
     }
 
     /**

plugins/FileQuota/FileQuota.php
@@ -69,6 +69,12 @@ select sum(at.size) as total
     where ua.gsactor_id = :actor_id and at.size is not null
 END;
 
+        $max_file_size = Common::config('attachments', 'file_quota');
+        if ($max_file_size < $filesize) {
+            throw new ClientException(_m('No file may be larger than {quota} bytes and the file you sent was {size} bytes. ',
+                                         ['quota' => $max_file_size, 'size' => $filesize]));
+        }
+
         $max_user_quota = Common::config('attachments', 'user_quota');
         if ($max_user_quota !== false) { // If not disabled
             $cache_key_user_total = "FileQuota-total-user-{$user_id}";

src/Entity/Note.php
@@ -25,6 +25,7 @@ use App\Core\Cache;
 use App\Core\DB\DB;
 use App\Core\Entity;
 use App\Core\Event;
+use App\Core\GSFile;
 use App\Core\VisibilityScope;
 use DateTimeInterface;
 
@@ -208,7 +209,12 @@ class Note extends Entity
     // @codeCoverageIgnoreEnd
     // }}} Autocode
 
-    public function getActorNickname()
+    public function getActor(): GSActor
+    {
+        return GSActor::getFromId($this->gsactor_id);
+    }
+
+    public function getActorNickname(): string
     {
         return GSActor::getNicknameFromId($this->gsactor_id);
     }
@@ -219,13 +225,14 @@ class Note extends Entity
         Event::handle('GetAvatarUrl', [$this->getGSActorId(), &$url]);
         return $url;
     }
+
     public static function getAllNotes(int $noteScope): array
     {
         return DB::sql('select * from note n ' .
-            'where n.reply_to is null and (n.scope & :notescope) <> 0 ' .
-            'order by n.created DESC',
-            ['n'         => 'App\Entity\Note'],
-            ['notescope' => $noteScope]
+                       'where n.reply_to is null and (n.scope & :notescope) <> 0 ' .
+                       'order by n.created DESC',
+                       ['n'         => 'App\Entity\Note'],
+                       ['notescope' => $noteScope]
         );
     }
 
@@ -288,6 +295,49 @@ class Note extends Entity
                                                'join note n with i.activity_id = n.id ' .
                                                'where n.id = :note_id and m.gsactor_id = :actor_id',
                                                ['note_id' => $this->id, 'actor_id' => $a->getId()]));
+    }
+
+    /**
+     * Create an instance of NoteToLink or fill in the
+     * properties of $obj with the associative array $args. Does
+     * persist the result
+     */
+    public static function create(array $args, mixed $obj = null): self
+    {
+        /** @var \Symfony\Component\HttpFoundation\File\UploadedFile[] $attachments */
+        $attachments = $args['attachments'];
+        unset($args['attachments']);
+
+        $note = parent::create($args, new self());
+
+        $processed_attachments = [];
+        foreach ($attachments as $f) {
+            Event::handle('EnforceUserFileQuota', [$f->getSize(), $args['gsactor_id']]);
+            $processed_attachments[] = [GSFile::sanitizeAndStoreFileAsAttachment($f), $f->getClientOriginalName()];
+        }
+
+        // Need file and note ids for the next step
+        DB::persist($note);
+
+        if ($processed_attachments != []) {
+            foreach ($processed_attachments as [$a, $fname]) {
+                if (DB::count('gsactor_to_attachment', $args = ['attachment_id' => $a->getId(), 'gsactor_id' => $args['gsactor_id']]) === 0) {
+                    DB::persist(GSActorToAttachment::create($args));
+                }
+                DB::persist(AttachmentToNote::create(['attachment_id' => $a->getId(), 'note_id' => $note->getId(), 'title' => $fname]));
+            }
+        }
+
+        return $note;
+    }
+
+    /**
+     * @return GSActor[]
+     */
+    public function getAttentionProfiles(): array
+    {
+        // TODO implement
+        return [];
     }
 
     public static function schemaDef(): array

components/Link/Link.php
@@ -22,6 +22,7 @@
 namespace Component\Link;
 
 use App\Core\DB\DB;
+use App\Core\Event;
 use App\Core\Modules\Component;
 use App\Entity;
 use App\Entity\NoteToLink;
@@ -38,26 +39,22 @@ class Link extends Component
 END;
 
     /**
-     * TODO PLACEHOLDER
+     * Extract URLs from $content and create the appropriate Link and NoteToLink entities
      */
     public function onProcessNoteContent(int $note_id, string $content)
     {
         if (Common::config('attachments', 'process_links')) {
-            $matched_urls   = [];
-            $processed_urls = false;
+            $matched_urls = [];
             preg_match_all(self::URL_REGEX, $content, $matched_urls, PREG_SET_ORDER);
             foreach ($matched_urls as $match) {
                 try {
                     $link_id = Entity\Link::getOrCreate($match[0])->getId();
                     DB::persist(NoteToLink::create(['link_id' => $link_id, 'note_id' => $note_id]));
-                    $processed_urls = true;
                 } catch (InvalidArgumentException) {
                     continue;
                 }
             }
-            if ($processed_urls) {
-                DB::flush();
-            }
         }
+        return Event::next;
     }
 }

components/Tag/Controller/Tag.php
@@ -1,0 +1,13 @@
+<?php
+
+namespace Component\Tag\Controller;
+
+use App\Core\Controller;
+
+class Tag extends Controller
+{
+    public function tag(string $tag)
+    {
+        dd($tag);
+    }
+}

components/Tag/Tag.php
@@ -1,0 +1,62 @@
+<?php
+
+// {{{ License
+
+// This file is part of GNU social - https://www.gnu.org/software/social
+//
+// GNU social is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GNU social is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with GNU social.  If not, see <http://www.gnu.org/licenses/>.
+
+// }}}
+
+namespace Component\Tag;
+
+use App\Core\DB\DB;
+use App\Core\Event;
+use App\Core\Modules\Component;
+use App\Entity\NoteTag;
+
+/**
+ * Component responsible for extracting tags from posted notes, as well as normalizing them
+ *
+ * @author Hugo Sales <hugo@hsal.es>
+ * @copyright 2021 Free Software Foundation, Inc http://www.fsf.org
+ * @license   https://www.gnu.org/licenses/agpl.html GNU AGPL v3 or later
+ */
+class Tag extends Component
+{
+    const TAG_REGEX = '/(?:^|\s)#([\pL\pN_\-\.]{1,64})/u'; // Brion Vibber 2011-02-23 v2:classes/Notice.php:367 function saveTags
+
+    /**
+     * Process note by extracting any tags present
+     */
+    public function onProcessNoteContent(int $note_id, string $content)
+    {
+        $matched_tags   = [];
+        $processed_tags = false;
+        preg_match_all(self::TAG_REGEX, $content, $matched_tags, PREG_SET_ORDER);
+        foreach ($matched_tags as $match) {
+            DB::persist($tag = NoteTag::create(['tag' => $match[0], 'note_id' => $note_id]));
+            $processed_tags = true;
+        }
+        if ($processed_tags) {
+            DB::flush();
+        }
+    }
+
+    public function onAddRoute($r): bool
+    {
+        $r->connect('tag', '/tag/{tag' . self::TAG_REGEX . '}' , [Controller\Tag::class, 'tag']);
+        return Event::next;
+    }
+}

config/bootstrap.php
@@ -41,6 +41,17 @@ define('GNUSOCIAL_CODENAME', 'Big bang');
 
 define('MODULE_CACHE_FILE', INSTALLDIR . '/var/cache/module_manager.php');
 
+/**
+ * StatusNet had this string as valid path characters: '\pN\pL\,\!\(\)\.\:\-\_\+\/\=\&\;\%\~\*\$\'\@'
+ * Some of those characters can be troublesome when auto-linking plain text. Such as "http://some.com/)"
+ * URL encoding should be used whenever a weird character is used, the following strings are not definitive.
+ */
+define('URL_REGEX_VALID_PATH_CHARS', '\pN\pL\,\!\.\:\-\_\+\/\@\=\;\%\~\*\(\)');
+define('URL_REGEX_VALID_QSTRING_CHARS', URL_REGEX_VALID_PATH_CHARS . '\&');
+define('URL_REGEX_VALID_FRAGMENT_CHARS', URL_REGEX_VALID_QSTRING_CHARS . '\?\#');
+define('URL_REGEX_EXCLUDED_END_CHARS', '\?\.\,\!\#\:\'');  // don't include these if they are directly after a URL
+define('URL_REGEX_DOMAIN_NAME', '(?:(?!-)[A-Za-z0-9\-]{1,63}(?<!-)\.)+[A-Za-z]{2,10}');
+
 // Work internally in UTC
 date_default_timezone_set('UTC');
 

plugins/Reply/Controller/Reply.php
@@ -57,7 +57,6 @@ class Reply extends Controller
         }
 
         $form = Form::create([
-
             ['content',     TextareaType::class, [
                 'label'      => _m('Reply'),
                 'label_attr' => ['class' => 'section-form-label'],
@@ -73,12 +72,11 @@ class Reply extends Controller
         if ($form->isSubmitted()) {
             $data = $form->getData();
             if ($form->isValid()) {
-                Posting::storeNote(
-                    actor_id: $actor_id,
-                    content_type: 'text/plain',
+                Posting::storeLocalNote(
+                    actor: $user->getActor(),
                     content: $data['content'],
+                    content_type: 'text/plain', // TODO
                     attachments: $data['attachments'],
-                    is_local: true,
                     reply_to: $reply_to,
                     repeat_of: null
                 );

diogogithub/gnu-social

commit 2bd19fa0874d8289bfd1080043925038adbc9624

[NOTE][Posting] Revert regressions introduced with c90efe2c52
Entity Note: It doesn't make sense to handle attachments on Note::create.
Attachments exist out of Notes, they are a thing on their own.
Furthermore, they aren't always handled the same, they most definitely
aren't always uploaded files.

FileQuota: It doesn't make sense to check if a file is greater than max
allowed upload size here. The plugin ensures a user is inside his
allowed quota, it's ignorant to anything else. Whether a file respect
max upload is a core thing that must be handled directly in the Posting
component. TODO: The configuration regarding user and monthly quota
must become FileQuotaPlugin settings and be removed from core.

c90efe2c52 - [UI] Add mechanism for rendering note contents in different formats. Implement plaintext rendering. Use rendered field for note content, rather than the content itself

components/Posting/Posting.php
@@ -28,7 +28,9 @@ use App\Core\Form;
 use function App\Core\I18n\_m;
 use App\Core\Modules\Component;
 use App\Entity\Actor;
+use App\Entity\ActorToAttachment;
 use App\Entity\Attachment;
+use App\Entity\AttachmentToNote;
 use App\Entity\Note;
 use App\Util\Common;
 use App\Util\Exception\ClientException;
@@ -109,6 +111,21 @@ class Posting extends Component
         return Event::next;
     }
 
+    /**
+     * Store the given note with $content and $attachments, created by
+     * $actor_id, possibly as a reply to note $reply_to and with flag
+     * $is_local. Sanitizes $content and $attachments
+     *
+     * @param Actor     $actor
+     * @param string    $content
+     * @param string    $content_type
+     * @param array     $attachments
+     * @param null|Note $reply_to
+     * @param null|Note $repeat_of
+     *
+     * @throws ClientException
+     * @throws ServerException
+     */
     public static function storeLocalNote(Actor $actor, string $content, string $content_type, array $attachments, ?Note $reply_to = null, ?Note $repeat_of = null)
     {
         $rendered = null;
@@ -118,11 +135,36 @@ class Posting extends Component
             'content'      => $content,
             'content_type' => $content_type,
             'rendered'     => $rendered,
-            'attachments'  => $attachments, // Not a regular field
             'is_local'     => true,
         ]);
+
+        $processed_attachments = [];
+        /** @var \Symfony\Component\HttpFoundation\File\UploadedFile[] $attachments */
+        foreach ($attachments as $f) {
+            $filesize      = $f->getSize();
+            $max_file_size = Common::config('attachments', 'file_quota');
+            if ($max_file_size < $filesize) {
+                throw new ClientException(_m('No file may be larger than {quota} bytes and the file you sent was {size} bytes. ' .
+                    'Try to upload a smaller version.', ['quota' => $max_file_size, 'size' => $filesize]));
+            }
+            Event::handle('EnforceUserFileQuota', [$filesize, $actor->getId()]);
+            $processed_attachments[] = [GSFile::sanitizeAndStoreFileAsAttachment($f), $f->getClientOriginalName()];
+        }
+
+        DB::persist($note);
+
+        // Need file and note ids for the next step
         Event::handle('ProcessNoteContent', [$note->getId(), $content, $content_type]);
         DB::flush();
+
+        if ($processed_attachments != []) {
+            foreach ($processed_attachments as [$a, $fname]) {
+                if (DB::count('actor_to_attachment', $args = ['attachment_id' => $a->getId(), 'actor_id' => $args['actor_id']]) === 0) {
+                    DB::persist(ActorToAttachment::create($args));
+                }
+                DB::persist(AttachmentToNote::create(['attachment_id' => $a->getId(), 'note_id' => $note->getId(), 'title' => $fname]));
+            }
+        }
     }
 
     public function onRenderNoteContent(string $content, string $content_type, ?string &$rendered, Actor $author, ?Note $reply_to = null)

plugins/FileQuota/FileQuota.php
@@ -69,12 +69,6 @@ select sum(at.size) as total
     where ua.actor_id = :actor_id and at.size is not null
 END;
 
-        $max_file_size = Common::config('attachments', 'file_quota');
-        if ($max_file_size < $filesize) {
-            throw new ClientException(_m('No file may be larger than {quota} bytes and the file you sent was {size} bytes. ',
-                                         ['quota' => $max_file_size, 'size' => $filesize]));
-        }
-
         $max_user_quota = Common::config('attachments', 'user_quota');
         if ($max_user_quota !== false) { // If not disabled
             $cache_key_user_total = "FileQuota-total-user-{$user_id}";

src/Entity/Note.php
@@ -25,7 +25,6 @@ use App\Core\Cache;
 use App\Core\DB\DB;
 use App\Core\Entity;
 use App\Core\Event;
-use App\Core\GSFile;
 use App\Core\VisibilityScope;
 use DateTimeInterface;
 
@@ -297,40 +296,6 @@ class Note extends Entity
                                                'join note n with i.activity_id = n.id ' .
                                                'where n.id = :note_id and m.actor_id = :actor_id',
                                                ['note_id' => $this->id, 'actor_id' => $a->getId()]));
-    }
-
-    /**
-     * Create an instance of NoteToLink or fill in the
-     * properties of $obj with the associative array $args. Does
-     * persist the result
-     */
-    public static function create(array $args, mixed $obj = null): self
-    {
-        /** @var \Symfony\Component\HttpFoundation\File\UploadedFile[] $attachments */
-        $attachments = $args['attachments'];
-        unset($args['attachments']);
-
-        $note = parent::create($args, new self());
-
-        $processed_attachments = [];
-        foreach ($attachments as $f) {
-            Event::handle('EnforceUserFileQuota', [$f->getSize(), $args['actor_id']]);
-            $processed_attachments[] = [GSFile::sanitizeAndStoreFileAsAttachment($f), $f->getClientOriginalName()];
-        }
-
-        // Need file and note ids for the next step
-        DB::persist($note);
-
-        if ($processed_attachments != []) {
-            foreach ($processed_attachments as [$a, $fname]) {
-                if (DB::count('actor_to_attachment', $args = ['attachment_id' => $a->getId(), 'actor_id' => $args['actor_id']]) === 0) {
-                    DB::persist(ActorToAttachment::create($args));
-                }
-                DB::persist(AttachmentToNote::create(['attachment_id' => $a->getId(), 'note_id' => $note->getId(), 'title' => $fname]));
-            }
-        }
-
-        return $note;
     }
 
     /**