15a87055a63bb1552a87af464ab01ebe046c8ac1
Hugo Sales <hugo@hsal.es>
2021-09-20 15:42:43 +0100

    [DOCKER][Redis] Add way to override Redis config. Default to only one database

--- docker/redis/docker-compose.fragment.sh
+++ docker/redis/docker-compose.fragment.sh
@@ -5,7 +5,10 @@ cat <<EOF
         image: redis:alpine
         restart: always
         tty: false
+        volumes:
+            - ./docker/redis/redis.conf:/etc/redis/redis.conf
         ports:
             - 6379:6379
+        command: redis-server /etc/redis/redis.conf
 
 EOF
