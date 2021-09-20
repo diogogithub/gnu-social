6cd86cac251c0be4555fb82382a2bf0a164b8ab3
Hugo Sales <hugo@hsal.es>
2021-09-20 16:17:13 +0100

    [COMPONENT][Tag] Add tag stream, with paging

--- components/Tag/Controller/Tag.php
+++ components/Tag/Controller/Tag.php
@@ -2,12 +2,36 @@
 
 namespace Component\Tag\Controller;
 
+use App\Core\Cache;
 use App\Core\Controller;
+use App\Core\DB\DB;
+use App\Util\Common;
+use Component\Tag\Tag as CompTag;
 
 class Tag extends Controller
 {
     public function tag(string $tag)
     {
-        dd($tag);
+        // TODO scope
+        $per_page = Common::config('streams', 'notes_per_page');
+        $page     = $this->int('page') ?: 1;
+        $tag      = CompTag::canonicalTag($tag);
+        $notes    = array_reverse( // TODO meme
+            Cache::getList(
+                "tag-{$tag}",
+                fn () => DB::dql(
+                    'select n from note n join note_tag nt with nt.note_id = n.id ' .
+                    'where nt.canonical = :tag order by nt.created ASC, n.id ASC',
+                    ['tag' => $tag]),
+                offset: $per_page * ($page - 1),
+                limit: $per_page - 1
+            )
+        );
+
+        return [
+            '_template' => 'tag_stream.html.twig',
+            'notes'     => $notes,
+            'page'      => $page,
+        ];
     }
 }
