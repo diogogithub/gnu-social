316723075b2f89a094fb1616373409e9019ac811
Hugo Sales <hugo@hsal.es>
2021-11-11 12:28:03 +0000

    [TESTS] Reload the initial table values after loading the data fixtures (as this purges the DB)

--- docker/tooling/coverage.sh
+++ docker/tooling/coverage.sh
@@ -4,6 +4,7 @@ cd /var/www/social || exit 1
 
 printf "Cleaning Redis cache: " && echo "FLUSHALL" | nc redis 6379
 yes yes | php bin/console doctrine:fixtures:load || exit 1
+php bin/console app:populate_initial_values # since loading fixtures purges the DB
 
 if [ "$#" -eq 0 ] || [ -z "$*" ]; then
     vendor/bin/simple-phpunit -vvv --coverage-html .test_coverage_report
